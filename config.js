416a2d16b0da86634ca53defbd4daa32efe53e5c
Jeremy Taylor <jeremy@Jeremys-MacBook-Pro.local>
2017-06-14 23:52:11 -0700

    changed config js file

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/restaurants-app';
-exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = 'mongodb://<jeremy>:<toby123>@ds149207.mlab.com:49207';
+exports.PORT = 49207;
